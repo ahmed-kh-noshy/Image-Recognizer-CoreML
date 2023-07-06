822c8deb9b33a1c74634a014e7d4f6e14d4ff9ac
ahmed khaled <ahmedkhaled5562.ak@gmail.com>
2023-07-06 15:09:43 +0300

    connect image view and camera button with view controller

--- Image Recognizer/ViewController.swift
+++ Image Recognizer/ViewController.swift
@@ -9,6 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    
+    @IBAction func cameraButton(_ sender: UIBarButtonItem) {
+        print("test...")
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
